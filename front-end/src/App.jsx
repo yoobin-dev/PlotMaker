7f2468cc1e0c67817c702bddaaac7fd7eb168391
Jowoomin <umin5056@naver.com>
2025-02-23 21:38:12 +0900

    prompt

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import "./styles/common.css";
 import "./styles/App.css";
 import SideMenuList from "./components/SideMenuList";
-import PlotListBox from "./components/PlotListBox";
+import PlotListPage from "./pages/PlotListPage";
+import PlotPromptPage from "./pages/PlotPromptPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="d-flex h-100">
+      <div id="" className="d-flex h-100">
         <SideMenuList nickName="CSS가 어려운 사람"></SideMenuList>
-        <PlotListBox></PlotListBox>
+        {/* <PlotListPage></PlotListPage> */}
+        <PlotPromptPage></PlotPromptPage>
+        {/* modalOn 클래스 부여하면 모달 배경 깔림 */}
+        <div id="modalBackground" className="bg_black_hover"></div>
       </div>
     </BrowserRouter>
   );
