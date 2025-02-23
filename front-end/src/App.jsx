27b39e996dc2a4254573386ebe65564c792073f6
yoobin-dev <ybleedev@gmail.com>
2025-02-23 21:56:44 +0900

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,10 @@ import { RouterProvider, useRoutes } from "react-router-dom";
 import root from "./routes/root.jsx";
 import "./styles/common.css";
 function App() {
-  return <RouterProvider route={root} />;
+
+  return (
+  <RouterProvider route={root} />
+  )
 }
 
 export default App;
