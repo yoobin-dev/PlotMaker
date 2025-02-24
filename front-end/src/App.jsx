b6bf17be0b16083529ba584cbe45b7b6884666b4
Jowoomin <umin5056@naver.com>
2025-02-24 20:33:24 +0900

    front

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,10 +2,7 @@ import { RouterProvider, useRoutes } from "react-router-dom";
 import root from "./routes/root.jsx";
 import "./styles/common.css";
 function App() {
-
-  return (
-  <RouterProvider route={root} />
-  )
+  return <RouterProvider router={root} />;
 }
 
 export default App;
--- front-end/src/routes/root.jsx
+++ front-end/src/routes/root.jsx
@@ -3,17 +3,15 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import NaverCallbackPage from "../pages/NaverCallbackPage";
 
-const root = createBrowserRouter(
-  [
-    {
-      path: '/login',
-      element: <LoginPage />
-    },
-    {
-      path: '/login/callback/naver',
-      element: <NaverCallbackPage />
-    }
-  ]
-)
+const root = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/login/callback/naver",
+    element: <NaverCallbackPage />,
+  },
+]);
 
 export default root;
