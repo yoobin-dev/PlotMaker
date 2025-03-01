a1653d8e384181389448a3c82a877c90fef935a8
Jowoomin <umin5056@naver.com>
2025-03-01 21:52:07 +0900

    plot make

--- front-end/src/components/modal/PlotInsertModal.jsx
+++ front-end/src/components/modal/PlotInsertModal.jsx
@@ -1,10 +1,12 @@
 function PlotInsertModal() {
   return (
-    <div id="plotModal">
-      <div className=""></div>
-      <div>
-        <div></div>
-        <div></div>
+    <div id="plotInsertModalBackground">
+      <div id="plotInsertModal">
+        <div className=""></div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
