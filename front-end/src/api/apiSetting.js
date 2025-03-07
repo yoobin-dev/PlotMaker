4e144a6e7ea636b73e3614b4769db8977a1bf4fb
Jowoomin <umin5056@naver.com>
2025-03-08 05:37:43 +0900

    SSL 인증 ㅜㅜㅜㅜ

--- front-end/src/api/apiSetting.js
+++ front-end/src/api/apiSetting.js
@@ -1,2 +1,8 @@
+/* 로컬용 : 개인 DB */
 // export const API_SERVER = "http://localhost:8080/api";
+
+/* 운영 DB */
 export const API_SERVER = "http://211.188.50.43:8080/api";
+
+/*  운영 배포용 (SSL 적용) */
+// export const API_SERVER = "/api";
