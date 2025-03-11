f871c7a0fde3ebdea648a005e7d97d1f2b6b1f92
Jowoomin <umin5056@naver.com>
2025-03-12 06:26:37 +0900

    finish build

--- front-end/src/api/apiSetting.js
+++ front-end/src/api/apiSetting.js
@@ -2,7 +2,7 @@
 // export const API_SERVER = "http://localhost:8080/api";
 
 /* 운영 DB */
-export const API_SERVER = "http://211.188.50.43:8080/api";
+// export const API_SERVER = "http://211.188.50.43:8080/api";
 
 /*  운영 배포용 (SSL 적용) */
-// export const API_SERVER = "/api";
+export const API_SERVER = "/api";
