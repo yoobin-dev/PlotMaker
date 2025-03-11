a5f7044c0f9dd022aaa8599ccf55a9e9b3b9c47e
yoobin-dev <ybleedev@gmail.com>
2025-03-11 16:42:05 +0900

    board api 수정

--- back-end/src/main/java/org/ohap/plotmaker/plot/ClovaService.java
+++ back-end/src/main/java/org/ohap/plotmaker/plot/ClovaService.java
@@ -63,4 +63,62 @@ public class ClovaService {
     }
   }
 
+  @SuppressWarnings("null")
+  public String requestClova2(String categoryCode, String messageContent){
+
+    List<Message> messages = new ArrayList<>();
+    String systemMsg = null;
+    switch (categoryCode) {
+      case "T000":
+        systemMsg = "";
+        break;
+      
+      case "T001":
+        systemMsg = "";
+        break;
+      
+      case "T002":
+        systemMsg = "";
+        break;
+    }
+
+    Message system = Message.builder().role("system").content(systemMsg).build();
+    messages.add(system);
+    Message input = Message.builder().role("system").content(messageContent).build();
+    messages.add(input);
+
+    ClovaRequestDTO request = ClovaRequestDTO.createWithApiKey(apiKey, messages);
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAll(request.getHeaders());
+    HttpEntity<ClovaRequestDTO> entity = new HttpEntity<>(request, headers);
+    try {
+      ResponseEntity<ClovaResponseDTO> response = restTemplate.exchange(
+        apiUrl, HttpMethod.POST, entity, ClovaResponseDTO.class);
+      
+      Message resMessage = response.getBody().getResult().getMessage();
+      StringBuilder sb = new StringBuilder();
+      sb.append(resMessage.getContent());
+
+      if(!categoryCode.equals("T000")){
+        while(sb.length() < 1800){
+          resMessage.setRole("assistant");
+          messages.add(resMessage);
+          messages.add(Message.builder().role("user").content("화수 표시나 제목없이 지금까지의 내용을 유지하면서 자연스럽게 이어지는 내용을 써줘.").build());
+          request = ClovaRequestDTO.createWithApiKey(apiKey, messages);
+          HttpEntity<ClovaRequestDTO> repeatEntity = new HttpEntity<>(request, headers);
+          ResponseEntity<ClovaResponseDTO> repeatRes = restTemplate.exchange(
+            apiUrl, HttpMethod.POST, repeatEntity, ClovaResponseDTO.class);
+          resMessage = repeatRes.getBody().getResult().getMessage();
+          sb.append(" " + repeatRes.getBody().getResult().getMessage().getContent());
+        }
+      }
+
+      String plot = sb.toString();
+      String plotContent = plot.substring(0, plot.lastIndexOf(".") + 1);
+      return plotContent;
+    } catch(Exception e){
+      throw new RuntimeException("CLOVA STUDIO 요청실패");
+    }
+  }
+
 }
