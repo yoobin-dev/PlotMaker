3dea760a76a5d92ac7073e48c08bf1ec589aa0a4
yoobin-dev <ybleedev@gmail.com>
2025-03-12 01:14:08 +0900

    회원탈퇴 구현

--- back-end/src/main/java/org/ohap/plotmaker/user/UserServiceImpl.java
+++ back-end/src/main/java/org/ohap/plotmaker/user/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package org.ohap.plotmaker.user;
 
+import org.ohap.plotmaker.login.LoginDTO;
+import org.ohap.plotmaker.mapper.LoginMapper;
 import org.ohap.plotmaker.mapper.UserMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
   private final UserMapper userMapper;
+  private final LoginMapper loginMapper;
   private final BCryptPasswordEncoder passwordEncoder;
 
   @Override
@@ -36,6 +40,19 @@ public class UserServiceImpl implements UserService {
     userMapper.insertUser(user);
     UserDTO userInfo = userMapper.findUserBySocialId(user.getSocialId());
     return userInfo;
+  }
+
+  @Override
+  public boolean isPasswordCorrect(String socialId, String userPw){
+    LoginDTO user = loginMapper.findLoginInfoBySocialId(socialId);
+    return passwordEncoder.matches(userPw, user.getUserPw());
+  }
+
+  @Transactional
+  @Override
+  public int deleteUser(String socialId){
+    int delete = userMapper.deleteUser(socialId);
+    return delete;
   }
   
 }
